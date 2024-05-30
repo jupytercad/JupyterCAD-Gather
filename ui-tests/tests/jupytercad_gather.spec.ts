3ba9fd500cd02f25502400faae3f8e40db3a75fc
Greg <gjmooney@asu.edu>
2024-05-30 10:00:24 +0200

    Linting

--- ui-tests/tests/jupytercad_gather.spec.ts
+++ ui-tests/tests/jupytercad_gather.spec.ts
@@ -16,6 +16,8 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(s => s === 'JupyterLab extension jupytercad_gather is activated!')
+    logs.filter(
+      s => s === 'JupyterLab extension jupytercad_gather is activated!'
+    )
   ).toHaveLength(1);
 });
