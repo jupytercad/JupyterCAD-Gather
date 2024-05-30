57421f18a559fb3df02cea2339d77ddf69e4faa8
Greg <gjmooney@asu.edu>
2024-05-30 14:18:46 +0200

    Setup pre-commit

--- ui-tests/jupyter_server_test_config.py
+++ ui-tests/jupyter_server_test_config.py
@@ -4,9 +4,10 @@
 opens the server to the world and provide access to JupyterLab
 JavaScript objects through the global window variable.
 """
+
 from jupyterlab.galata import configure_jupyter_server
 
-configure_jupyter_server(c)
+configure_jupyter_server(c)  # noqa: F821
 
 # Uncomment to set server log level to debug level
 # c.ServerApp.log_level = "DEBUG"
